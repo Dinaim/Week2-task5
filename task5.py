1fbbbd3692123f09eba29863d816c2467431f239
Dinaim <dinaim.zholdoshova@gmail.com>
2020-07-16 22:43:23 +0600

    task5

---
+++ task5.py
@@ -0,0 +1,21 @@
+guests = ["Nurayim", "Dastan", "Janar"]
+letter = "You are invited to home party "
+# print(letter + guests[0])
+# print(letter + guests[1])
+# print(letter + guests[2])
+
+b = guests.insert(0, "Asan")
+# print(guests)
+
+c = guests.insert(2, "Begimai")
+# print(guests)
+
+d = guests.append("Ainura")
+print(guests)
+
+print(letter + guests[0])
+print(letter + guests[1])
+print(letter + guests[2])
+print(letter + guests[3])
+print(letter + guests[4])
+print(letter + guests[5])
